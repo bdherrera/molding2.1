75364f0ac29454782ee20a7082629ce9f159fca3
Sindi Rodriguez <99542046+SIMIRO1109@users.noreply.github.com>
2022-12-11 09:05:13 -0500

    adicion modulos inventario y clintes

---
+++ src/main/java/com/mycompany/molding3/service/ClientesFacade.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.molding3.service;
+
+import com.mycompany.molding3.model.Clientes;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Dell
+ */
+@Stateless
+public class ClientesFacade extends AbstractFacade<Clientes> {
+
+    @PersistenceContext(unitName = "com.mycompany_molding3_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ClientesFacade() {
+        super(Clientes.class);
+    }
+    
+}
