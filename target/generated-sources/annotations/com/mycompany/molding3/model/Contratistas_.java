2b557021e90b470144d6e60bc7c3139a4db3fc2e
kevin <kevv_12@hotmail.com>
2022-12-15 20:40:37 -0500

    la buena mostro(base de modulo)

--- target/generated-sources/annotations/com/mycompany/molding3/model/Contratistas_.java
+++ target/generated-sources/annotations/com/mycompany/molding3/model/Contratistas_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-15T16:28:11")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-15T17:57:59")
 @StaticMetamodel(Contratistas.class)
 public class Contratistas_ { 
 
