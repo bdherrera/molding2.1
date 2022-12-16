2f0b9ca604a0e1ab30d0d05c765e54d402633a4d
Sindi Rodriguez <99542046+SIMIRO1109@users.noreply.github.com>
2022-12-15 22:26:56 -0500

    Creacion de xhtml y controller de orden de entrada

---
+++ target/generated-sources/annotations/com/mycompany/molding3/model/Materiales_.java
@@ -0,0 +1,16 @@
+package com.mycompany.molding3.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-15T21:48:50")
+@StaticMetamodel(Materiales.class)
+public class Materiales_ { 
+
+    public static volatile SingularAttribute<Materiales, String> estado;
+    public static volatile SingularAttribute<Materiales, Integer> idMateriales;
+    public static volatile SingularAttribute<Materiales, String> cantidad;
+    public static volatile SingularAttribute<Materiales, String> nombre;
+
+}
---
+++ target/generated-sources/annotations/com/mycompany/molding3/model/Usuarios_.java
@@ -0,0 +1,22 @@
+package com.mycompany.molding3.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-15T21:48:50")
+@StaticMetamodel(Usuarios.class)
+public class Usuarios_ { 
+
+    public static volatile SingularAttribute<Usuarios, String> fechanacimiento;
+    public static volatile SingularAttribute<Usuarios, Integer> cedula;
+    public static volatile SingularAttribute<Usuarios, String> apellido;
+    public static volatile SingularAttribute<Usuarios, String> correo;
+    public static volatile SingularAttribute<Usuarios, String> direccion;
+    public static volatile SingularAttribute<Usuarios, Integer> idusarios;
+    public static volatile SingularAttribute<Usuarios, Integer> telefono;
+    public static volatile SingularAttribute<Usuarios, String> nombre;
+    public static volatile SingularAttribute<Usuarios, Integer> edad;
+    public static volatile SingularAttribute<Usuarios, String> contraseña;
+
+}
