f00cd5e8533e6c3c4b7d30befec4a456a954211a
kevin <kevv_12@hotmail.com>
2022-12-15 20:50:30 -0500

    cambios actualizar kevin, la re buena

---
+++ target/generated-sources/annotations/com/mycompany/molding3/model/OrdenEntrada_.java
@@ -0,0 +1,18 @@
+package com.mycompany.molding3.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-15T17:57:59")
+@StaticMetamodel(OrdenEntrada.class)
+public class OrdenEntrada_ { 
+
+    public static volatile SingularAttribute<OrdenEntrada, String> nombreProveedor;
+    public static volatile SingularAttribute<OrdenEntrada, String> fechaCompra;
+    public static volatile SingularAttribute<OrdenEntrada, String> ubicacion;
+    public static volatile SingularAttribute<OrdenEntrada, Integer> idOrdenEntrada;
+    public static volatile SingularAttribute<OrdenEntrada, String> pago;
+    public static volatile SingularAttribute<OrdenEntrada, String> nombreMaterial;
+
+}
