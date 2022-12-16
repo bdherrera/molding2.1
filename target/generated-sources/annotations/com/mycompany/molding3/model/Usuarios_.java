fe109608b25a054075d73d4d542e8874520d7d30
kevin <kevv_12@hotmail.com>
2022-12-16 14:55:08 -0500

    actualizar

--- target/generated-sources/annotations/com/mycompany/molding3/model/Usuarios_.java
+++ target/generated-sources/annotations/com/mycompany/molding3/model/Usuarios_.java
@@ -4,23 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-16T01:22:11")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-16T00:00:53")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-15T21:48:50")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-15T19:58:08")
-=======
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-15T17:57:59")
->>>>>>> c0de487c572460f723b95799c7cafd08d170bbc2
->>>>>>> 2febb5e0bb5a40f38d9e189d25a6fba0c4612cde
->>>>>>> 0a467f7ebb39bd73dff12e9e6c825155cf31c470
->>>>>>> 8731cb2f6a4abd28fdebe376c7bf27cd53589b66
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-16T02:34:43")
 @StaticMetamodel(Usuarios.class)
 public class Usuarios_ { 
 
