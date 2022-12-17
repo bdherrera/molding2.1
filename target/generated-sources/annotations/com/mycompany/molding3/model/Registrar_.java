91715db5c0c8d5203f55b26b7d0a72f5430d8eea
kevin <kevv_12@hotmail.com>
2022-12-16 21:46:05 -0500

    actualizacion pag registro

--- target/generated-sources/annotations/com/mycompany/molding3/model/Registrar_.java
+++ target/generated-sources/annotations/com/mycompany/molding3/model/Registrar_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-16T16:07:24")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-16T17:05:08")
 @StaticMetamodel(Registrar.class)
 public class Registrar_ { 
 
--- target/generated-sources/annotations/com/mycompany/molding3/model/Usuarios_.java
+++ target/generated-sources/annotations/com/mycompany/molding3/model/Usuarios_.java
@@ -4,35 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-16T16:07:24")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-16T02:34:43")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-15T22:36:33")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-16T01:22:11")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-16T00:00:53")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-15T21:48:50")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-15T19:58:08")
-=======
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-15T17:57:59")
->>>>>>> c0de487c572460f723b95799c7cafd08d170bbc2
->>>>>>> 2febb5e0bb5a40f38d9e189d25a6fba0c4612cde
->>>>>>> 0a467f7ebb39bd73dff12e9e6c825155cf31c470
->>>>>>> 8731cb2f6a4abd28fdebe376c7bf27cd53589b66
->>>>>>> a26d9037dd03a3280d2798662a5a318960b2bfd0
->>>>>>> a2a0f461f1fcb2e41ca1644ce9fd2049ad4a14ce
->>>>>>> 0c8061199947e323d0f77f503c1f9197e1b233fe
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-12-16T17:05:08")
 @StaticMetamodel(Usuarios.class)
 public class Usuarios_ { 
 
